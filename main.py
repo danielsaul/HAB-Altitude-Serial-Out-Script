1b234ca8bcc61ecd53526619804a238bd56ea7a6
unknown <Daniel@.(none)>
2011-10-15 23:12:02 +0100

    Gets altitude from Habitat

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import json
 import urllib2
+import serial
 
-#Get data from Habitat
-data = urllib2.urlopen('http://habitat.habhub.org/habitat/_design/habitat/_view/payload_telemetry?startkey=[%22ALPHA%22,%22latest%22]&descending=true&limit=1&include_docs=true')
-data = json.load(data)
+def getAltitude():
+    #Get data from Habitat
+    data = urllib2.urlopen('http://habitat.habhub.org/habitat/_design/habitat/_view/payload_telemetry?startkey=[%22ALPHA%22,%22latest%22]&descending=true&limit=1&include_docs=true')
+    habitatdata = json.load(data)
+    data.close()
 
-data.close()
+    altitude = habitatdata['rows'][0]['doc']['data']['altitude']
+    return altitude
+
+currentAltitude = getAltitude()
+
+
+
