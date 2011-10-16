5742751fadb89a90964b470f54c2908f15dfdd10
Daniel Saul <daniel@dansaul.co.uk>
2011-10-16 11:09:59 +0100

    finished script

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import json
 import urllib2
 import serial
+import time
+
+port = '\\\.\CNCA0'
+baud = 19200
+qualifier = "HELLO"
+payload = "BUZZ"
+prevAltitude = 0
+
+serout = serial.Serial(port,baud,timeout=2,writeTimeout=2)
+
 
 def getAltitude():
     #Get data from Habitat
-    data = urllib2.urlopen('http://habitat.habhub.org/habitat/_design/habitat/_view/payload_telemetry?startkey=[%22ALPHA%22,%22latest%22]&descending=true&limit=1&include_docs=true')
+    try:
+        data = urllib2.urlopen('http://habitat.habhub.org/habitat/_design/habitat/_view/payload_telemetry?startkey=[%22{0}%22,%22latest%22]&descending=true&limit=1&include_docs=true'.format(payload))
+    except: return prevAltitude
     habitatdata = json.load(data)
     data.close()
 
-    altitude = habitatdata['rows'][0]['doc']['data']['altitude']
+    try:
+        altitude = habitatdata['rows'][0]['doc']['data']['altitude']
+    except: return prevAltitude
     return altitude
 
-currentAltitude = getAltitude()
+add_nulls = lambda number, zero_count : "{0:0{1}d}".format(number, zero_count)
 
-
-
+while serout.writable() == True:
+    currentAltitude = getAltitude()
+    currentAltitude = '%0*d' % (5, currentAltitude)
+    if currentAltitude != prevAltitude:
+        serout.write(bytes(qualifier))
+        serout.write(bytes(currentAltitude))
+        print currentAltitude
+    prevAltitude = currentAltitude
+raw_input("Press ENTER to exit")
